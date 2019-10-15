016d226af74db607786c937871cfbbd8a2cd4e95
LavrovSergey <54863977+LavrovSergey@users.noreply.github.com>
2019-10-15 22:20:25 +0300

    1

---
+++ Classwork3.cpp
@@ -0,0 +1,66 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int n;
+	cout << "Введите размер матрицы" << endl;
+	cin >> n;
+	int**mas = new int* [n];
+	for (int i = 0; i < n; ++i)
+		mas[i] = new int[n];
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			cout << "Заполните матрицу ";
+			cin >> mas[i][j];
+		}
+	}
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			cout << mas[i][j] << " \t";
+		}
+		cout << endl;
+	}
+	for (int j = 0; j < n; j++)
+	{
+		int sum1 = 0;
+		bool flag = true;
+		for (int i = 0; i < n; i++)
+		{
+			sum1 += mas[i][j];
+			if (mas[i][j] < 0) { flag = false; }
+		}
+		if (flag == true)
+
+		{
+			cout << "Сумма " << " в столбце " << j + 1 << " = " << sum1<<endl;
+		}
+	}
+		int summin;
+		for (int i = 0; i < n; i++)
+		{
+			int sum = 0;
+			for (int j = 0; j <= i; j++)
+			{
+				sum += abs(mas[i - j][j]);
+				if ((i == 0) || (summin > sum))summin = sum;
+			}
+		}
+		for (int i = 1; i < n; i++)
+		{
+			int sum = 0;
+			for (int j = 0; j < n - i; j++)sum += abs(mas[i + j][n - i - j]);
+			if (summin > sum)summin = sum;
+		}
+		cout << "Минимум среди сумм модулей элементов диагоналей параллельных побочной диагонали матрицы = "<<summin;
+
+	
+	return 0;
+}
+
